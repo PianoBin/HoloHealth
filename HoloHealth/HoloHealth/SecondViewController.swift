93775520224b3539c8352acbac5b0dfe1e18d02f
Shoji Moto <shmoto1@gmail.com>
2017-10-14 19:35:11 -0400

    tabbed bar controller

---
+++ HoloHealth/HoloHealth/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  HoloHealth
+//
+//  Created by Shoji Moto on 10/14/17.
+//  Copyright © 2017 Shoji Moto. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
